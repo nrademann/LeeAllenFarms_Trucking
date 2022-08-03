ee604032dfae0368e41a5a84a933720285ad6e51
nick rademann <nrademann@hotmail.com>
2022-08-03 13:02:25 -0500

    All and add still have problems

---
+++ LeeAllenFarms_Trucking/LeeAllenFarms_Trucking/Controllers/ClientController.cs
@@ -0,0 +1,34 @@
+﻿using LeeAllenFarms_Trucking;
+using LeeAllenFarms_Trucking.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeeAllenFarms_Trucking.Controllers
+{
+    public class ClientController : Controller
+    {
+        ClientDbContext db;
+        public ClientController(ClientDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult AllClient()
+        {
+            return View(db.Clients);
+        }
+        public IActionResult AddClient()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddClient(Client client)
+        {
+            db.Add(client);
+            db.SaveChanges();
+            return RedirectToAction("AllClient");
+        }
+/*        public IActionResult Index()
+        {
+            return View();
+        }
+ */   }
+}
